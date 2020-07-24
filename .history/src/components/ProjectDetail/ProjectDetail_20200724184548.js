9a15a2316060c32baf2f18c9d756e173fef0537a
Akinsiku Femi <akinsiku13@gmail.com>
2020-07-24 23:16:43 +0100

    Implement react player

---
+++ .history/src/components/ProjectDetail/ProjectDetail_20200724184548.js
@@ -0,0 +1,119 @@
+import React, { useContext, useRef } from 'react';
+import '../../App.scss';
+import { ModalContext } from '../../contexts/ModalContext';
+
+
+
+export default function ProjectDetail({ project }){
+    
+
+    const videoRef = useRef()
+
+
+
+    let { showModal, setShowModal } = useContext(ModalContext);
+    console.log(showModal + 'from detail');
+    // console.log(loading)
+    if (!project)
+    {
+
+        return (
+            <div style={{ display: 'none' }}>Loading...</div>
+
+        )
+    }
+
+    const closeModal = () => {
+        setShowModal(false)
+        if(videoRef.current.onpause === null)
+        console.log(videoRef)
+    }
+
+    const handlePaused = () => {
+        console.log('you just paused');
+    }
+
+
+    return (
+
+        <div className={showModal ? `modal` : `hide-modal`}>
+            <div style={{ background: `${project.color}` }} className="shadow-lg  modal-content">
+                <div className="header-modal">
+                    <div className="header-modal-text">
+                        <p className="project-name">{project.name}</p>
+                        <p style={{ color: project.color }} className="theme-name">{project.theme}</p>
+                    </div>
+                    <div className="icon-modal">
+                        <span style={{ color: 'white' }} onClick={closeModal} className="close">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><line x1="15" y1="9" x2="9" y2="15"></line><line x1="9" y1="9" x2="15" y2="15"></line></svg>
+
+                        </span>
+
+                        <a style={{ color: 'white' }} className="close-btn" href={project.hostedlink} target="_">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="15 3 21 3 21 9"></polyline><polyline points="9 21 3 21 3 15"></polyline><line x1="21" y1="3" x2="14" y2="10"></line><line x1="3" y1="21" x2="10" y2="14"></line></svg>
+                        </a>
+                    </div>
+
+                </div>
+
+
+
+                <div className="inner-content ">
+
+
+                    <div className="vidd">
+                        <div class="iframe-container">
+                            <iframe onPause={() => console.log('paused')} ref={videoRef} title="youtube-video" width="560" height="315" src={project.videoLink} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                    </div>
+
+
+
+                </div>
+                <div className="text-project">
+                    <div className="pt-5">
+                        <h2 className="font-weight-bold">About :</h2>
+                        <p className="h3 info">{project.about}</p>
+                    </div>
+
+                    <div>
+                        <h2 className="font-weight-bold">Facebook Open source used :</h2>
+                        <p className="h3 info">{project.stackused.map(stack => stack)}</p>
+                    </div>
+                    <div>
+                        <h2 className="font-weight-bold">Nationality :</h2>
+                        <p className="h3 info">{project.nationality.map((country, i) => <li key={i}>{country}</li>)}</p>
+                    </div>
+                    <div>
+                        <h2 className="font-weight-bold">Team Members :</h2>
+                        <p className="h3 info">{
+                            project.members.map((item, i) => (
+                                <ul>
+                                    <li key={i}>{item}</li>
+                                </ul>
+                            ))
+                        }</p>
+                        <a href={`mailto:${project.email}`} style={{ cursor: 'pointer', fontSize: '2rem' }} className="btn-info font-weight-bold btn-lg">Email the builder</a>
+                    </div>
+                </div>
+
+
+
+
+
+            </div>
+        </div>
+
+
+
+
+    )
+
+
+
+}
+
+
+
+// 
+
---
+++ .history/src/pages/Home_20200724220056.js
@@ -0,0 +1,161 @@
+import React, { useEffect, useContext, useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../App.scss';
+
+import Aos from 'aos';
+import "aos/dist/aos.css";
+import Footer from '../components/Footer/Footer';
+import { ModeContext } from '../contexts/ModeContext';
+import { ModalContext } from '../contexts/ModalContext';
+import { ProjectContext } from '../contexts/context';
+
+import ProjectList from '../components/ProjectList/ProjectList';
+import ProjectDetail from '../components/ProjectDetail/ProjectDetail';
+import sdg from '../images/sdg.png';
+import andela from '../images/andela.png';
+import ThemeContainer from '../components/ThemeContainer';
+
+import ReactPlayer from 'react-player';
+
+const Home = () => {
+    
+    useEffect(() => {
+        Aos.init({
+            duration: 2000
+        })
+    },[])
+
+    let { darkMode } = useContext(ModeContext)
+    let { featuredProjects : projects, loading }= useContext(ProjectContext);
+    console.log(loading)
+    let [selectedProject, setSelectedProject] = useState(null);
+
+    const [showModal, setShowModal] = useState(false)
+
+
+    const onProjectSelect = (project) => {
+        setSelectedProject(project);
+
+    }
+
+    
+
+    return (
+        <ModalContext.Provider value={{ showModal, setShowModal }}>
+            
+            <div>
+               
+                <div className="bg-header">
+                    <div className="page-head">
+                        <div className="sponsors">
+                            <span style={{ textAlign: 'center' }}> <img className="andela" src={andela} alt="andela" /> &nbsp; </span> <span className="facebook">FACEBOOK</span>
+                        </div>
+                            
+                        <p className="build-container">#BUILDF<span><img className="sdg-small" src={sdg} alt="sdg" />R</span> <span >SDG</span></p>
+                        <p className="challenge animate__animated animate__pulse">CHALLENGE <span className="year">2020</span></p>   
+                                
+                    </div>
+
+
+                </div>
+
+
+
+                
+                
+                <div className={darkMode ? `dark-mode bg-section2` : `light-mode bg-section2`}>
+                    <div className="container ">
+                        <div className="row">
+                            <div className="col-12 col-lg-9 my-5 intro-container">
+                                <p className={darkMode ? `color-light`: `color-dark`}>The <span style={{ color: '#0fddff' }}>#BuildforSDG</span> Challenge is a Facebook and Andela partnership initiative to up-skill developers across Sub-Saharan (Kenya, Uganda, Cameroon, Ghana, Nigeria and South-Africa) through a six-week immersive program where selected developers worked in agile remote teams to innovate on projects relating to the SDGs.</p>
+                                <p className={darkMode ? `color-light` : `color-dark`}>The <span style={{ color: '#0fddff' }}>#BuildforSDG</span> program is committed to helping empower you to build real-world, locally and relevant solutions focused on the Sustainable Development Goals(SDGs) while gaining skills you need to advance your career.</p>
+                                <p className={darkMode ? `color-light` : `color-dark`}>The <span style={{ color: '#0fddff' }}>Goal of the #BuildforSDG Challenge 2020</span> is to empower developers to build global, high-value, high-impact projects to help improve their market-readiness while learning within the community.</p>
+                            </div>
+                        </div>
+
+                    </div> 
+                </div>
+
+                
+                
+
+
+                <div className={darkMode ? `dark-mode` : `light-mode `}>
+                    <div className="container ">
+                        <div className="row">
+                            <h2 className="top-projects  my-5">Top 60 Projects</h2>
+                        </div>
+                        <div  className="row">
+                            <div className="col-12 col-lg-9 ">
+                                <p className={darkMode ? `color-light intro-container ` : `color-dark intro-container`}>With over 175 team projects in the works, the Innovators of the #BuildforSDG Program are building solutions ranging from Food Bank regulators to simulated concepts for SMART city building as well as improved revenue collection tech models for communities and standardized waste collection solutions.</p>
+                                <p className={darkMode ? `color-light intro-container` : `color-dark intro-container`}>These innovators while working in teams are building tech solutions that could make life a lot easier for communities across Sub-Saharan Africa battling the Covid-19 Pandemic. Over 160 teams from #BuildforSDG Challenge are leveraging on Open Source Technologies-- Top-picks and go to choices for these Innovators amongst these Open Source technologies include React, React Native, Jest, Flow, Facebook Authentication, and Graph QL.</p>
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+                
+
+                <div className={darkMode ? `dark-mode` : `light-mode`}>
+                    <div className="container ">
+                        <div className="row">
+                            <h2 className={darkMode ? `color-light theme-header my-4` : `color-dark theme-header my-4`}>PROJECTS BY SDG</h2>
+                        </div>
+
+                        <ThemeContainer/>
+                        
+                 
+                    </div>
+                </div>
+
+                <div className={darkMode ? `dark-mode` : `light-mode`}>
+                    <div className="container py-5">
+
+                        
+                        <ProjectDetail project={selectedProject} />
+
+                        {/* <Loading/> */}
+
+                        <ProjectList projects={projects} onProjectSelect={onProjectSelect} />
+
+                    </div>
+                </div>    
+
+                <div className={darkMode ? `dark-mode` : `light-mode`}>
+                    <div className="container ">
+                        <div className="row twobuttons">
+                            <div >
+                                <Link className="judges-btn" to="/judges">Meet the judges →</Link>
+                                <Link className="projects-btn" to="/projects">See all projects →</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                
+                {/*  */}
+                <ReactPlayer>
+                    
+                </ReactPlayer>
+
+
+                {/*  */}
+                
+               
+                <Footer />
+                    
+                 
+
+
+            </div>
+        </ModalContext.Provider>  
+
+
+            
+        )
+}
+
+
+
+export default Home;
